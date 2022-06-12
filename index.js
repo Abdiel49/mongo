d8814f15d677c0d5ac80ca142c91aca08a41173f
abdiel49 <abdielorellana3@gmail.com>
2022-06-12 18:13:50 -0400

    refactor project estructure rputes

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ const express = require('express');
 const cors = require('cors');
 
 // midelwares for express
+const noteRoute = require('./src/routes/noteRoutes');
 const notFound = require('./src/midelware/notFound');
 const handleErrors = require('./src/midelware/handleErrors');
-
-// Mongo Models for mongoose
-const Note = require('./src/models/Note');
 
 
 const PORT = process.env.PORT || 3000;
@@ -22,72 +20,11 @@ app.get('/', (_, res) => {
   res.send('Hello World!');
 });
 
-app.get('/api/notes', (req, res) => {
-  Note.find({})
-      .then((notes) => {
-        if (notes) {
-          res.send(notes);
-        } else {
-          res.status(400).send([]);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json({error: err});
-      });
-});
+// route midelware for express
+app.use('/api/notes', noteRoute);
 
-app.get('/api/notes/:id', (req, res, next) => {
-  const {id} = req.params;
-  Note.findById(id)
-      .then((note) => {
-        if (note) {
-          res.send(note);
-        } else {
-          res.status(404).end();
-        }
-      })
-      .catch((err) => next(err) );
-});
-
-app.post('/api/notes', (req, res) => {
-  const body = req.body;
-  const note = new Note(body);
-  note.save()
-      .then((result) => {
-        res.json(result);
-      })
-      .catch((err) => {
-        console.log('error on POST method note', err);
-        res.status(500).json({error: err});
-      });
-});
-
-app.put('/api/notes/:id', (req, res, next) => {
-  const {id} = req.params;
-  const body = req.body;
-
-  const newNote = {
-    contend: body.contend,
-    important: body.important,
-  };
-
-  Note.findByIdAndUpdate(id, newNote, {new: true})
-      .then((note) => res.json(note))
-      .catch((err) => next(err));
-});
-
-app.delete('/api/notes/:id', (req, res, next) => {
-  const {id} = req.params;
-  Note.findByIdAndRemove(id)
-      .then((result) => {
-        res.status(204).json(result);
-      })
-      .catch((err) => next(err));
-});
-
+// midelware exceptions
 app.use(notFound);
-
 app.use(handleErrors);
 
 app.listen(PORT, () => {
